7901d597e3e6ccbe80d5901a0afa895b69ecadb3
Tyler Fox <Tyler_Fox@intuit.com>
2014-03-28 09:41:09 -0700

    Add example project

---
+++ LocationManagerExample/LocationManagerExample/INTUViewController.m
@@ -0,0 +1,173 @@
+//
+//  INTUViewController.m
+//  LocationManagerExample
+//
+//  Copyright (c) 2014 Intuit Inc.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the
+//  "Software"), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "INTUViewController.h"
+#import "INTULocationManager.h"
+
+@interface INTUViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeoutLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *desiredAccuracyControl;
+@property (weak, nonatomic) IBOutlet UISlider *timeoutSlider;
+@property (weak, nonatomic) IBOutlet UIButton *requestCurrentLocationButton;
+@property (weak, nonatomic) IBOutlet UIButton *cancelRequestButton;
+@property (weak, nonatomic) IBOutlet UIButton *forceCompleteRequestButton;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+@property (assign, nonatomic) INTULocationAccuracy desiredAccuracy;
+@property (assign, nonatomic) NSTimeInterval timeout;
+
+@property (assign, nonatomic) NSInteger locationRequestID;
+
+@end
+
+@implementation INTUViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    self.desiredAccuracyControl.selectedSegmentIndex = 0;
+    self.desiredAccuracy = INTULocationAccuracyCity;
+    self.timeoutSlider.value = 10.0;
+    self.timeout = 10.0;
+    
+    self.locationRequestID = NSNotFound;
+    self.statusLabel.text = @"Tap the button below to start a new location request.";
+}
+
+/**
+ Callback when the "Request Current Location" button is tapped.
+ */
+- (IBAction)startLocationRequest:(id)sender
+{
+    __weak __typeof(self) weakSelf = self;
+    
+    INTULocationManager *locMgr = [INTULocationManager sharedInstance];
+    self.locationRequestID = [locMgr requestLocationWithDesiredAccuracy:self.desiredAccuracy
+                                                                timeout:self.timeout
+                                                                  block:^(CLLocation *currentLocation, INTULocationAccuracy achievedAccuracy, INTULocationStatus status) {
+                                                                      __typeof(weakSelf) strongSelf = weakSelf;
+                                                                      
+                                                                      if (status == INTULocationStatusSuccess) {
+                                                                          // achievedAccuracy is at least the desired accuracy (potentially better)
+                                                                          strongSelf.statusLabel.text = [NSString stringWithFormat:@"Location request successful! Current Location:\n%@", currentLocation];
+                                                                      }
+                                                                      else if (status == INTULocationStatusTimedOut) {
+                                                                          // You may wish to inspect achievedAccuracy here to see if it is acceptable, if you plan to use currentLocation
+                                                                          strongSelf.statusLabel.text = [NSString stringWithFormat:@"Location request timed out. Current Location:\n%@", currentLocation];
+                                                                      }
+                                                                      else {
+                                                                          // An error occurred
+                                                                          if (status == INTULocationStatusServicesNotDetermined) {
+                                                                              strongSelf.statusLabel.text = @"Error: User has not responded to the permissions alert.";
+                                                                          } else if (status == INTULocationStatusServicesDenied) {
+                                                                              strongSelf.statusLabel.text = @"Error: User has denied this app permissions to access device location.";
+                                                                          } else if (status == INTULocationStatusServicesRestricted) {
+                                                                              strongSelf.statusLabel.text = @"Error: User is restricted from using location services by a usage policy.";
+                                                                          } else if (status == INTULocationStatusServicesDisabled) {
+                                                                              strongSelf.statusLabel.text = @"Error: Location services are turned off for all apps on this device.";
+                                                                          } else {
+                                                                              strongSelf.statusLabel.text = @"An unknown error occurred.\n(Are you using iOS Simulator with location set to 'None'?)";
+                                                                          }
+                                                                      }
+                                                                      
+                                                                      strongSelf.locationRequestID = NSNotFound;
+                                                                  }];
+}
+
+/**
+ Callback when the "Force Complete Request" button is tapped.
+ */
+- (IBAction)forceCompleteRequest:(id)sender
+{
+    [[INTULocationManager sharedInstance] forceCompleteLocationRequest:self.locationRequestID];
+}
+
+/**
+ Callback when the "Cancel Request" button is tapped.
+ */
+- (IBAction)cancelRequest:(id)sender
+{
+    [[INTULocationManager sharedInstance] cancelLocationRequest:self.locationRequestID];
+    self.locationRequestID = NSNotFound;
+    self.statusLabel.text = @"Location request cancelled.";
+}
+
+- (IBAction)desiredAccuracyControlChanged:(UISegmentedControl *)sender
+{
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            self.desiredAccuracy = INTULocationAccuracyCity;
+            break;
+        case 1:
+            self.desiredAccuracy = INTULocationAccuracyNeighborhood;
+            break;
+        case 2:
+            self.desiredAccuracy = INTULocationAccuracyBlock;
+            break;
+        case 3:
+            self.desiredAccuracy = INTULocationAccuracyHouse;
+            break;
+        case 4:
+            self.desiredAccuracy = INTULocationAccuracyRoom;
+            break;
+        default:
+            break;
+    }
+}
+
+- (IBAction)timeoutSliderChanged:(UISlider *)sender
+{
+    self.timeout = round(sender.value);
+    self.timeoutLabel.text = [NSString stringWithFormat:@"Timeout: %ld seconds", (long)self.timeout];
+}
+
+/**
+ Implement the setter for locationRequestID in order to update the UI as needed.
+ */
+- (void)setLocationRequestID:(NSInteger)locationRequestID
+{
+    _locationRequestID = locationRequestID;
+    
+    BOOL isProcessingLocationRequest = (locationRequestID != NSNotFound);
+    
+    self.desiredAccuracyControl.enabled = !isProcessingLocationRequest;
+    self.timeoutSlider.enabled = !isProcessingLocationRequest;
+    self.requestCurrentLocationButton.enabled = !isProcessingLocationRequest;
+    self.forceCompleteRequestButton.enabled = isProcessingLocationRequest;
+    self.cancelRequestButton.enabled = isProcessingLocationRequest;
+    
+    if (isProcessingLocationRequest) {
+        [self.activityIndicator startAnimating];
+        self.statusLabel.text = @"Location request in progress...";
+    } else {
+        [self.activityIndicator stopAnimating];
+    }
+}
+
+@end
